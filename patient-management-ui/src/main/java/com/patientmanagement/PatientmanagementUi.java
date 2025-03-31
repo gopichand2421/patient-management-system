eb0dea5b4ada13bf7b7b8a5427dfbda0bdc2735a
gopichand <gopichand2416@gmail.com>
2025-04-01 00:34:09 +0530

    intial commit

---
+++ patient-management-ui/src/main/java/com/patientmanagement/PatientmanagementUi.java
@@ -0,0 +1,11 @@
+package com.patientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientmanagementUi {
+    public static void main(String[] args) {
+        SpringApplication.run(PatientmanagementUi.class, args);
+    }
+}
